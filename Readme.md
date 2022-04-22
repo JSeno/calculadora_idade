38302f6f69986cf57e0daf2209e7f582a9512380
Jefferson Seno <jefferson.seno@datacake.com.br>
2022-04-22 11:56:20 -0300

    Iniciando a calculadora de idades

---
+++ Readme.md
@@ -0,0 +1,11 @@
+Calculadora de idade
+
+Minha continuação da aulas do tkinter.
+
+    
+    
+    []: # Language: markdown
+    []: # Path: Readme.md
+
+
+Estarei fazendo uma calculadora onde você entra com a data de nascimento e a data escolhida e o retorno será a idade.
---
+++ main.py
@@ -0,0 +1,16 @@
+# Importando a biblioteca tkinter
+from tkinter import *
+from tkinter import ttk
+
+# -- Criação da janela principal --
+janela = Tk() # Criando a janela principal
+janela.title("Calculadora de Idade") # Titulo da janela
+janela.geometry("310x400") # Tamanho da janela
+
+
+
+
+
+
+
+janela.mainloop() # Loop infinito da janela
